b00171be95d1e36d3eb8e9dffd1c3fdf2ef09092
Practical-1 <rp6821915@gmail.com>
2022-08-24 21:30:02 +0530

    Q-1.1

---
+++ src/main/kotlin/Q-1.kt
@@ -0,0 +1,38 @@
+fun main(){
+    print("Enter an integer : ")
+    val myNumber = readLine()!!
+
+    print("Enter a Float Value : ")
+    val myFloat = readLine()
+
+    print("Enter Character : ")
+    val myLetter = readLine()
+
+    print("Enter String value : ")
+    val myText = readLine()
+
+    print("Enter Boolean value : ")
+    val myBoolean = readLine()
+
+    print("Enter Double value : ")
+    val myDouble = readLine()
+
+    print("Enter Long value : ")
+    val myLong = readLine()
+
+    print("Enter Short value : ")
+    val myShort = readLine()
+
+    print("Enter Byte value : ")
+    val myByte = readLine()
+
+    println("Integer Value : $myNumber")
+    println("Float Value : $myFloat")
+    println("Character value : $myLetter")
+    println("String value : $myText")
+    println("Boolean value : $myBoolean")
+    println("double value : $myDouble")
+    println("Long value : $myLong")
+    println("Short value : $myShort")
+    println("Byte value : $myByte")
+}
