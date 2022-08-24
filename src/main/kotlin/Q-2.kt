2dbe5212db7246579f76008336c3f26a7e5d8b1b
Practical-1 <rp6821915@gmail.com>
2022-08-24 21:30:21 +0530

    Q-1.3

---
+++ src/main/kotlin/Q-2.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    var int = 20
+    val double : Double = int.toDouble()
+    print(double)
+
+    var string = "123"
+    val int1 : Int = string.toInt()
+    println(int1)
+
+    var double1 : Double = string.toDouble()
+    println(double1)
+}
---
+++ src/main/kotlin/Q-3.kt
@@ -0,0 +1,37 @@
+fun main(){
+    print("Enter enrollment number : ")
+    val en = readLine()
+
+    print("Enter Name : ")
+    var name = readLine()
+
+    print("Enter Branch : ")
+    var br = readLine()
+
+    print("Enter Class : ")
+    var cl = readLine()
+
+    print("Enter batch : ")
+    var bt = readLine()
+
+    print("Enter College name : ")
+    var clg = readLine()
+
+    print("Enter University name : ")
+    var uni = readLine()
+
+    print("Enter age : ")
+    var age = readLine()
+
+    println("***********************************************")
+
+    println("Student Enrollment number : $en")
+    println("Student Name : $name")
+    println("Student branch : $br")
+    println("Student Class : $cl")
+    println("Student batch : $bt")
+    println("Student college name : $clg")
+    println("Student university name : $uni")
+    println("Student age : $age")
+
+}
